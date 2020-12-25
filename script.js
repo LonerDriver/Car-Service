b5459d4d7c67bbcda9848321072c2c46785ff0d4
LonerDriver <61434366+LonerDriver@users.noreply.github.com>
2020-12-25 13:54:21 +0400

    add script

---
+++ script.js
@@ -0,0 +1,37 @@
+const form = document.forms[0];
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    new FormData(form);
+});
+
+form.addEventListener("formdata", event => {
+    const data = event.formData;
+    filled = true;
+
+    const entries = [...data.entries()];
+    console.log(entries);
+
+    const values = [...data.values()];
+    console.log(values);
+
+   values.forEach(element => {
+    if (element == "") {
+        filled = false;
+    }
+   });
+
+    if (filled) {
+        Swal.fire(
+            'Принято в обработку!',
+            'Ваша заявка вскоре будет рассмотрена!',
+            'Скоро с вами свяжется наш специалист.'
+          )
+    } else {
+        Swal.fire(
+            'Ошибка! Данные не заполнены.',
+            'Заполните форму данными для заявки!'
+          )
+    }
+    
+  });
